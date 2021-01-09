56e3da8f5bef2cb2a43cec73b5da0f1bac4575ff
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2021-01-09 15:58:44 +0900

    Changing from master to main in travis deploy

--- .deploy.sh
+++ .deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]
+if [ "$TRAVIS_BRANCH" = "main" -a "$TRAVIS_PULL_REQUEST" = "false" ]
 then
     python3 -m pip install twine
     python3 -m twine upload dist/*
